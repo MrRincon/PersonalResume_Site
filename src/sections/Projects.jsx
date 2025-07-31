a76a11811c53b2a107a301dc598666d7a9336f83
Alam Rincon <alamrdrgz98@gmail.com>
2025-07-31 20:54:19 +0100

    Finishing with Project section and other sections adjustments

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -88,12 +88,12 @@ function Projects({ USER }) {
                 )}
               </div>
 
-              <button
+              {/* <button
                 disabled
                 className="bg-gray-600 text-white px-4 py-2 rounded-full opacity-50 cursor-not-allowed"
               >
                 See Skills Achieved (Coming Soon)
-              </button>
+              </button> */}
             </div>
           ))}
         </div>
